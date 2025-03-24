6bb7727e627711f8a36de2f4adadcd5409d0c28e
matteozucchetti <dev@matteozucchetti.it>
2025-03-24 21:30:16 +0100

    redirect staging vs localhost

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,4 +3,9 @@ import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Google],
+  callbacks: {
+    async redirect({ redirectTo }) {
+      return redirectTo;
+    },
+  },
 });
