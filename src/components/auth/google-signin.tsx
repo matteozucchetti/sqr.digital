ddf32576c1f84d8cd2616939b1d52f47e2ae79f7
matteozucchetti <dev@matteozucchetti.it>
2025-03-29 12:47:21 +0100

    fixed redirect for login

--- src/components/auth/google-signin.tsx
+++ src/components/auth/google-signin.tsx
@@ -8,7 +8,9 @@ export function GoogleSignin() {
 
   return (
     <Button
-      onClick={() => signIn("google", { redirectTo: "/admin" })}
+      onClick={() =>
+        signIn("google", { redirectTo: `${window.location.origin}/admin` })
+      }
       variant="outline"
     >
       Sign in with Google
